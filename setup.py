cfb293d6a0e32745cb2bd7fe8a28fddb64ca9167
Ravi Pathak <r31618@eondata.onmicrosoft.com>
2019-03-26 17:03:21 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -7,33 +7,15 @@ from setuptools import setup
 package_name = 'printhello'
 filename = package_name + '.py'
 
-
-def get_version():
-    import ast
-
-    with open(filename) as input_file:
-        for line in input_file:
-            if line.startswith('__version__'):
-                return ast.parse(line).body[0].value.s
-
-
-def get_long_description():
-    try:
-        with open('readme.txt', 'r') as f:
-            return f.read()
-    except IOError:
-        return ''
-
-
 setup(
     name=package_name,
-    version=get_version(),
+    #version=get_version(),
     author='ravi',
     author_email='ravipathak.de@gmail.com',
     description='test python snap',
     url='https://github.com/ravipath/testpythonsnapping.git',
     scripts=['printhello.py']
-    long_description=get_long_description(),
+    #long_description=get_long_description(),
     py_modules=[package_name],
     license='License :: OSI Approved :: MIT License',
 )
